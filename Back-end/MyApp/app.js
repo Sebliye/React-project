6f29d89332c64a190a1f740c716152b201a101af
Seble Aygoda <saygoda@mum.edu>
2020-04-15 23:12:02 -0500

    last one

--- Back-end/MyApp/app.js
+++ Back-end/MyApp/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const jwt=require('jsonwebtoken');
-
+//const session=require('express-session');
+//const MongoDBStore = require('connect-mongo')(session);
 const prdRoute = require('./routes/router.product');
 const userRoute=require('./routes/router.user');
 const uaaMiddleware = require('./Jwt/UaaMiddleware');
@@ -13,15 +14,37 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+// app.use(
+//   session({
+//     secret: 'my secret',
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store
+//   })
+// )
 app.use(uaaMiddleware.authenticate);
 
 app.use(prdRoute);
 app.use(userRoute);
 
-mongoose.connect('mongodb://localhost:27017/shiromeda', {            
-    useNewUrlParser: true, useUnifiedTopology: true }) 
-    .then(() => { 
-        app.listen(8080, ()=>{
-            console.log('server 8081 is start!! ');
-        });
-    }).catch(err => console.error(err));
+// const store=new MONGODB_URL({
+//   uri:process.env.MONGODB_URL,
+//   collection:'sessions'
+// });
+const MONGO_URL=`mongodb+srv://seble:<password>@cluster0-ehp1r.mongodb.net/test?retryWrites=true&w=majority`
+// const MONGODB_URL=`mongodb+srv://${process.env.MONGO_USER}:${
+// process.env.MONGO_PWD}@cluster0-oxo7g.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+app.listen(process.env.PORT || 8081);
+}).catch(err=>console.log(err));
+
+
+// mongoose.connect('mongodb://localhost:27017/shiromeda', {            
+//     useNewUrlParser: true, useUnifiedTopology: true }) 
+//     .then(() => { 
+//         app.listen(8080, ()=>{
+//             console.log('server 8081 is start!! ');
+//         });
+//     }).catch(err => console.error(err));
